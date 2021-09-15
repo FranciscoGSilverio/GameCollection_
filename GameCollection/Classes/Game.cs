b18ee3863ae6e2e29d395e9618cefc681696ae4e
Francisco Silvério <franciscogsilverio@gmail.com>
2021-09-14 21:57:32 -0300

    Added the game collection project

---
+++ GameCollection/Classes/Game.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameCollection.Classes
+{
+	class Game
+	{
+		private string title;
+		private int year;
+		private string gender;
+		private int maxPlayers;
+		private int metaCritic;
+
+		public Game(string title, int year, string gender, int maxPlayers, int metaCritic)
+		{
+			this.title = title;
+			this.year = year;
+			this.gender = gender;
+			this.maxPlayers = maxPlayers;
+			this.metaCritic = metaCritic;
+		}
+
+		//Setters
+		public void setTitle(string title)
+		{
+			this.title = title;
+		}
+		public void setYear(int year)
+		{
+			this.year = year;
+		}
+		public void setGender(string gender)
+		{
+			this.gender = gender;
+		}
+		public void setMaxPlayers(int maxPlayers)
+		{
+			this.maxPlayers = maxPlayers;
+		}
+		public void setMetaCritic(int metaCritic)
+		{
+			this.metaCritic = metaCritic;
+		}
+
+		//Getters
+		public string getTitle()
+		{
+			return this.title;
+		}
+		public int getYear()
+		{
+			return this.year;
+		}
+		public string getGender()
+		{
+			return this.gender;
+		}
+		public int getMaxPlayers()
+		{
+			return this.maxPlayers;
+		}
+		public int getMetaCritic()
+		{
+			return this.metaCritic;
+		}
+	}
+}
---
+++ GameCollection/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using GameCollection.Classes;
+using System.Collections.Generic;
+
+namespace GameCollection
+{
+    class Program
+    {
+        static List<Game> gamesList = new List<Game>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("### Biblioteca de jogos ###");
+                Console.WriteLine("\n");
+                Console.WriteLine("1 - Adicionar um jogo");
+                Console.WriteLine("2 - Listar os jogos");
+                Console.WriteLine("3 - Atualizar um jogo");
+                Console.WriteLine("4 - Deletar um jogo");
+                Console.WriteLine("");
+
+                int option = Console.Read();
+                switch (option)
+                {
+                    case '1':
+                        gamesList.Add(AddingGames());
+                        break;
+
+                    case '2':
+                        listingGames();
+                        break;
+
+                    case '3':
+                        updatingGames();
+                        break;
+
+                    case '4':
+                        removingGames();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+            }
+        }
+        static private Game AddingGames()
+        {
+            Console.Clear();
+
+            Console.WriteLine("ADICIONAR JOGO");
+            Console.ReadLine();
+
+            string name = "";
+            while (name.Length == 0)
+            {
+                Console.Write("Nome: ");
+                name = Console.ReadLine();
+            }
+
+            Console.Write("Lançamento: ");
+            int data = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Gênero: ");
+            string gender = Console.ReadLine();
+
+            Console.Write("Max. de jogadores: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Metacritic: ");
+            int metacritic = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+
+            Game game = new Game(name, data, gender, max, metacritic);
+
+            Console.Beep();
+            Console.WriteLine("Precione qualquer tecla para voltar ao menu");
+            Console.ReadLine();
+            Console.Clear();
+
+            return game;
+        }
+
+        static private void listingGames()
+        {
+            Console.Clear();
+            Console.WriteLine("JOGOS ADICIONADOS");
+            Console.WriteLine("");
+
+            foreach (Game game in gamesList)
+            {
+                Console.Write("Jogo: ");
+                Console.WriteLine(game.getTitle());
+
+                Console.Write("Ano: ");
+                Console.WriteLine(game.getYear());
+
+                Console.Write("Gênero: ");
+                Console.WriteLine(game.getGender());
+
+                Console.Write("Max. de jogadores: ");
+                Console.WriteLine(game.getMaxPlayers());
+
+                Console.Write("MetaCritic: ");
+                Console.WriteLine(game.getMetaCritic());
+                Console.WriteLine("--------------------");
+                Console.WriteLine("");
+
+            }
+            Console.WriteLine("Precione qualquer tecla para prosseguir....");
+            Console.ReadLine();
+            Console.WriteLine("");
+        }
+
+        static private void updatingGames()
+        {
+            Console.Clear();
+ 
+            listingGames();
+
+            Console.WriteLine("ATUALIZANDO INFO. DOS JOGOS");
+            Console.WriteLine("");
+
+            Console.Write("Escolha o jogo a ser editado. Digite 1, 2, 3....: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nome editado: ");
+            string editedName = Console.ReadLine();
+
+            Console.Write("Lançamento editado: ");
+            int editedYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Genero editado: ");
+            string editedGender = Console.ReadLine();
+
+            Console.Write("Max. de jogadores editado: ");
+            int editedMax = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Metacritic editado: ");
+            int editedMetacritic = Convert.ToInt32(Console.ReadLine());
+
+            gamesList[choice-1].setTitle(editedName);
+            gamesList[choice-1].setYear(editedYear);
+            gamesList[choice-1].setGender(editedGender);
+            gamesList[choice-1].setMaxPlayers(editedMax);
+            gamesList[choice-1].setMetaCritic(editedMetacritic);
+
+            Console.Clear();
+        }
+
+         static private void removingGames()
+        {
+            Console.Clear();
+            
+            listingGames();
+
+            Console.WriteLine("REMOVENDO JOGOS");
+            Console.WriteLine("");
+
+            Console.Write("Escolha o jogo a ser removido. Digite 1, 2, 3....: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            gamesList.RemoveAt(choice - 1);
+            Console.Clear();
+        }
+    }
+}
